c0b8f7f3a500cf58d40e67d9a7bae9a114cf4a75
DESKTOP-V8SE2NK\Intern_02 <razom.info@gmail.com>
2021-11-30 16:43:19 +0200

    fixed work version

---
+++ ConverterPixel/ConverterPixel/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ConverterPixel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void calc()
+        {
+            double sample;
+
+            var deviceWidth = stackLayout.Width;
+            var deviceHeight = stackLayout.Height;
+
+            var trueParse = double.TryParse(entryWidthLayout.Text, out sample);
+            trueParse &= double.TryParse(entryHeightLayout.Text, out sample);
+            trueParse &= double.TryParse(entryInputPixel.Text, out sample);
+
+            if (trueParse)
+            {
+                var widthLayout = double.Parse(entryWidthLayout.Text);
+                var heightLayout = double.Parse(entryHeightLayout.Text);
+                var inputPixel = double.Parse(entryInputPixel.Text);
+
+                var scaleX = deviceWidth / widthLayout;
+                var scaleY = deviceHeight / heightLayout;
+
+                if (switcher.IsChecked)
+                {
+                    var tmp = (scaleX * inputPixel).ToString();
+
+                    labelResult.Text = tmp.Length > 8 ? tmp.Substring(0, 8) : tmp;
+                }
+                else
+                {
+                    var tmp = (scaleY * inputPixel).ToString();
+
+                    labelResult.Text = tmp.Length > 8 ? tmp.Substring(0, 8) : tmp;
+                }
+
+            }
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            calc();
+        }
+
+        private void Entry_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            calc();
+        }
+
+        private void Entry_TextChanged_2(object sender, TextChangedEventArgs e)
+        {
+            calc();
+        }
+
+        private void switch_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            calc();
+        }
+    }
+}
