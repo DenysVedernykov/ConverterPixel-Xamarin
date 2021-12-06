4a5a0ee6c54f37a0011150e452684fcbf099235b
Denys Vedernykov <razom.info@gmail.com>
2021-12-06 23:51:34 +0200

    fixed bag with SafeArea

--- ConverterPixel/ConverterPixel/MainPage.xaml.cs
+++ ConverterPixel/ConverterPixel/MainPage.xaml.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 namespace ConverterPixel
 {
@@ -12,6 +8,8 @@ namespace ConverterPixel
     {
         public MainPage()
         {
+            On<iOS>().SetUseSafeArea(true);
+
             InitializeComponent();
         }
 
